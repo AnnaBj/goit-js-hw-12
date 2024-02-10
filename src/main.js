9f2078716b909f01fc61ad104ee3441af4856471
AnnaBjerkholt <anja.bjkt@gmail.com>
2024-02-10 21:53:44 +0100

    finished

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
@@ -5,87 +6,168 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const lightBox = new SimpleLightbox('.gallery-link');
 
-const form = document.querySelector('.form');
-const gallery = document.querySelector('.gallery');
-const loader = document.querySelector('.loader');
-loader.style.display = 'none';
-const searchParams = {
-  key: '42296556-10e657c6d09903018112d9d9d',
-  q: '',
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: true,
-};
+const formElement = document.querySelector('.form');
+const galleryElement = document.querySelector('.gallery');
+const loaderElement = document.querySelector('.loader');
+const loadBtnElement = document.querySelector('.more');
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  loader.style.display = 'inline-block';
-  gallery.innerHTML = '';
-  const inputText = form.elements.search.value.trim();
-  searchParams.q = inputText;
-  fetchImage()
-    .then(images => renderGallery(images))
-    .catch(error => console.log(error));
+let currentPage = 1;
+let itemsPerPage = 15;
+let currentSearchQuery;
+loadBtnElement.style.display = 'none';
+loaderElement.style.display = 'none';
 
-  form.reset();
-});
-
-function fetchImage() {
-  const urlparams = new URLSearchParams(searchParams);
-  return fetch(`https://pixabay.com/api/?${urlparams}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+async function fetchImages(searchQuery, page) {
+  const queryParams = {
+    key: '42296556-10e657c6d09903018112d9d9d',
+    q: searchQuery,
+    page,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: itemsPerPage,
+  };
+  const urlParams = new URLSearchParams(queryParams);
+  const { data } = await axios.get(`https://pixabay.com/api/?${urlParams}`);
+  return data;
 }
 
-function renderGallery(images) {
-  if (images.hits.length === 0) {
+formElement.addEventListener('submit', async e => {
+  e.preventDefault();
+  currentPage = 1;
+  loadBtnElement.style.display = 'none';
+  galleryElement.innerHTML = '';
+  currentSearchQuery = formElement.elements.search.value.trim();
+  if (currentSearchQuery === '') {
     iziToast.show({
-      message:
-        'Sorry, there are no images matching your search query. Please try again!',
+      message: 'Please write search image',
       messageColor: '#FAFAFB',
       backgroundColor: '#EF4040',
       position: 'topRight',
     });
-  } else {
-    const item = images.hits
-      .map(
-        image => `<li class="gallery-item">
-    <a class="gallery-link" href="${image.largeImageURL}" >
-      <img
-        class="gallery-image"
-        src="${image.webformatURL}"
-        alt="${image.tags}"
-      width = "360"
-      />
-    </a>
-    <div class="img-text">
-    <div class="img-info">
-    <h3>Likes</h3>
-    <p> ${image.likes}</p>
-    </div>
-    <div class="img-info">
-    <h3>Views</h3>
-    <p> ${image.views}</p>
-    </div>
-       <div class="img-info">
-    <h3>Comments</h3>
-    <p> ${image.comments}</p>
-    </div>
-       <div class="img-info">
-    <h3>Downloads</h3>
-    <p> ${image.downloads}</p>
-    </div>
-      </div>
-  </li>`
-      )
-      .join('');
-
-    gallery.innerHTML = item;
-   
-    lightBox.refresh();
+    return;
   }
-  loader.style.display = 'none';
+
+  loaderElement.style.display = 'inline-block';
+  try {
+    const { hits, totalHits } = await fetchImages(
+      currentSearchQuery,
+      currentPage
+    );
+    if (totalHits === 0) {
+      iziToast.show({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        messageColor: '#FAFAFB',
+        backgroundColor: '#EF4040',
+        position: 'topRight',
+      });
+      loaderElement.style.display = 'none';
+      return;
+    }
+    displayImages(hits);
+    if (totalHits < itemsPerPage) {
+      showNotification();
+    } else {
+      loadBtnElement.style.display = 'block';
+    }
+  } catch (error) {
+    iziToast.show({
+      message: `Sorry, ${error}`,
+      messageColor: '#FAFAFB',
+      backgroundColor: '#EF4040',
+      position: 'topRight',
+    });
+  } finally {
+    formElement.reset();
+  }
+});
+
+loadBtnElement.addEventListener('click', async () => {
+  currentPage += 1;
+
+  loaderElement.style.display = 'inline-block';
+  try {
+    const { hits, totalHits } = await fetchImages(
+      currentSearchQuery,
+      currentPage
+    );
+    displayImages(hits);
+    scrollPage();
+    if (itemsPerPage * currentPage > totalHits) {
+      showNotification();
+    }
+  } catch (error) {
+    iziToast.show({
+      message: `Sorry, ${error}`,
+      messageColor: '#FAFAFB',
+      backgroundColor: '#EF4040',
+      position: 'bottomCenter',
+    });
+  }
+});
+
+function displayImages(images) {
+  const htmlMarkup = images
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img class="gallery-image" src="${webformatURL}" alt="${tags}" width="360" />
+        </a>
+        <div class="img-text">
+          <div class="img-info">
+            <h3>Likes</h3>
+            <p>${likes}</p>
+          </div>
+          <div class="img-info">
+            <h3>Views</h3>
+            <p>${views}</p>
+          </div>
+          <div class="img-info">
+            <h3>Comments</h3>
+            <p>${comments}</p>
+          </div>
+          <div class="img-info">
+            <h3>Downloads</h3>
+            <p>${downloads}</p>
+          </div>
+        </div>
+      </li>
+    `
+    )
+    .join('');
+
+  galleryElement.insertAdjacentHTML('beforeend', htmlMarkup);
+  lightBox.refresh();
+  loaderElement.style.display = 'none';
+}
+
+function showNotification() {
+  iziToast.show({
+    message: 'We are sorry, but you have reached the end of search results.',
+    messageColor: '#FAFAFB',
+    backgroundColor: '#1DB8F5',
+    position: 'topRight',
+  });
+  loadBtnElement.style.display = 'none';
+  loaderElement.style.display = 'none';
+}
+
+function scrollPage() {
+  const listItem = document.querySelector('.gallery-item');
+  const scrollHeight = listItem.getBoundingClientRect().height * 2;
+  window.scrollBy({
+    top: scrollHeight,
+    left: 0,
+    behavior: 'smooth',
+  });
 }
